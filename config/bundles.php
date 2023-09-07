6ce46c09631be38761fc427f3e7b9e6ed2bc1c1c
lsprezak <lsprezak@gmail.com>
2023-09-07 23:22:34 +0200

    install composer require lexik/jwt-authentication-bundle composer require gesdinet/jwt-refresh-token-bundle
    and configuration jwt login

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,6 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
