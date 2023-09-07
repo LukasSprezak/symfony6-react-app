7887e6984c1c9e5bdb9acaef710831fa48320484
lsprezak <lsprezak@gmail.com>
2023-09-08 00:30:30 +0200

    addition of further statuses for products and roles

--- src/Enum/RoleEnum.php
+++ src/Enum/RoleEnum.php
@@ -6,6 +6,15 @@ namespace App\Enum;
 
 enum RoleEnum: string
 {
-    case ROLE_ADMIN = 'ROLE_ADMIN';
     case ROLE_USER = 'ROLE_USER';
+
+    case ROLE_CUSTOMER = 'ROLE_CUSTOMER';
+
+    case ROLE_EMPLOYEE = 'ROLE_EMPLOYEE';
+
+    case ROLE_MERCHANT = 'ROLE_MERCHANT';
+
+    case ROLE_ADMIN = 'ROLE_ADMIN';
+
+    case ROLE_SUPER_ADMIN = 'ROLE_SUPER_ADMIN';
 }
--- src/Enum/StatusProductEnum.php
+++ src/Enum/StatusProductEnum.php
@@ -6,11 +6,21 @@ namespace App\Enum;
 
 enum StatusProductEnum: string
 {
-    case DRAFT = 'DRAFT';
-    case PUBLISHED = 'PUBLISHED';
+    case IN_PREPARATION = 'IN_PREPARATION';
+
+    case IN_PROGRESS = 'IN_PROGRESS';
+
+    case ORDER_CANCELLED = 'ORDER_CANCELLED';
+
+    case COMPLETED = 'COMPLETED';
+
+    case SENT_TO_CUSTOMER = 'SENT_TO_CUSTOMER';
+
+    case RETRIEVED = 'RETRIEVED';
+
     case FINISHED = 'FINISHED';
 
-    public function isStatusProduct(?string $status): bool
+    public function isStatusProduct(null|string $status): bool
     {
         return $this->value === $status;
     }
